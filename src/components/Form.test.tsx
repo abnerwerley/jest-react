317deca4b9e8f22df7319c8dd76715d819d9accf
Abner Werley <abnerwerley77@gmail.com>
2023-01-26 09:55:48 -0300

    :white_check_mark: @abnerwerley [Test to form added]

---
+++ src/components/Form.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import Form from "./Form";
+
+test("when input is empty, new participants cannot be added", () => {
+  render(<Form />);
+  //find in DOM the input
+  const input = screen.getByPlaceholderText("Insert participants' names");
+  //find the button
+  const button = screen.getByRole("button");
+
+  //ensure that the input is in the document
+  expect(input).toBeInTheDocument();
+  //ensure that the button is disabled
+  expect(button).toBeDisabled();
+});
